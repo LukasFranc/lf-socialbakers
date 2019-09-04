e438890c17dfb8d12348397699ac84150a9bf161
Lukáš Franc <lf@antstudio.cz>
2019-09-04 22:19:54 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var $ = gulpLoadPlugins({pattern: '*'});
+
+
+
+gulp.task('build:sass', function () {
+  return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/css/main.scss'])
+    .pipe($.sass())
+    .pipe($.concat('project.css'))
+    .pipe(gulp.dest('src/dist/css/'))
+})
+
+gulp.task('build:js', function () {
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js','node_modules/tether/dist/js/tether.min.js','node_modules/bootstrap/dist/js/bootstrap.min.js'])
+    .pipe($.uglify())
+    .pipe($.concat('all.min.js'))
+    .pipe(gulp.dest('src/dist/js/'))
+})
